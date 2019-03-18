9a931a681f9c6fcd8ac959835a148505c4be65ce
akinsola <thomas.lawanson@gmail.com>
2019-03-18 11:39:42 +0000

    updated projects components

--- src/app/components/projects/project/project.component.ts
+++ src/app/components/projects/project/project.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { IProject } from '../../../interfaces/IProject';
+import { ActivatedRoute } from '@angular/router';
+import { ProjectService } from '../../../services/project.service';
 
 @Component({
   selector: 'app-project',
@@ -6,10 +9,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./project.component.scss']
 })
 export class ProjectComponent implements OnInit {
-
-  constructor() { }
+  project: IProject;
+  constructor(private route: ActivatedRoute, private projectService: ProjectService) { }
 
   ngOnInit() {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.project = this.projectService.getProject(id);
   }
 
 }
