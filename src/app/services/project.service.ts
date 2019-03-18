aa29d82e216ab74fb2eba282dc20482a3c114d72
akinsola <thomas.lawanson@gmail.com>
2019-03-18 11:38:29 +0000

    added functions to projects folder

--- src/app/services/project.service.ts
+++ src/app/services/project.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { IProject } from '../interfaces/IProject';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProjectService {
-
+  private projects: IProject[] = [
+    {
+      id: 'contract',
+      title: 'Contract App',
+      description: 'This is a contract app',
+      github_url: 'string',
+      url: 'string'
+    }
+  ]
   constructor() { }
+
+  getAllProjects = (): IProject[] => {
+    return this.projects;
+  }
+
+  getProject = (id: string): IProject => {
+    return this.projects.find((project) => {
+      return project.id === id;
+    });
+  }
 }
