63258b3d8618abf10cb623416932a930e2ede451
akinsola <thomas.lawanson@gmail.com>
2019-05-12 11:14:11 +0100

    reformatted nav bar to load headers dynamically
    also added some scss colour changes

--- src/app/components/partials/navbar/navbar.component.ts
+++ src/app/components/partials/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IHeader } from '../../../interfaces/IHeader';
 
 @Component({
   selector: 'app-navbar',
@@ -6,7 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
-
+  private headers :IHeader[] = [
+    {
+      link: 'projects',
+      title: 'projects'
+    },
+    {
+      link: 'videos',
+      title: 'videos'
+    },
+    {
+      link: 'games',
+      title: 'games'
+    },
+  ];
   constructor() { }
 
   ngOnInit() {
